3137822c125432af99062dc1d6e3887cb28fcd1d
jordanPR <146361533+jordanPRdanvekin@users.noreply.github.com>
2024-10-04 09:51:57 +0100

    animamos mas
    añadimos mas animaciones

--- Assets/scripts/Leantweenejemplos.cs
+++ Assets/scripts/Leantweenejemplos.cs
@@ -12,6 +12,8 @@ public class Leantweenejemplos : MonoBehaviour
     GameObject texto;
     [SerializeField]
     LeanTweenType curva;
+    [SerializeField]
+    GameObject cubo;
 
     public float time = 0.29f;
 
@@ -28,11 +30,49 @@ public class Leantweenejemplos : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            background.SetActive(true);
-            background2.SetActive(true);
-            texto.SetActive(true);
-            LeanTween.moveLocalY(texto, -900f, 0f);
-            LeanTween.moveLocalY(texto, 0f, time).setEase(curva);
+            if (texto.activeSelf)
+            {
+            HidePopup();
+            }
+        else
+            {
+                ShowPopUp();
+            }
+           
         }
     }
+    public void ShowPopUp()
+    {
+        background.SetActive(true);
+        LeanTween.moveLocalY(background, 900f, 0f);
+        LeanTween.moveLocalY(background, 0f, time).setEase(curva);
+        background2.SetActive(true);
+        LeanTween.moveLocalX(background2, -9000f, 0f);
+        LeanTween.moveLocalX(background2, 0f, time).setEase(curva);
+        texto.SetActive(true);
+        LeanTween.moveLocalY(texto, -900f, 0f);
+        LeanTween.moveLocalY(texto, 0f, time).setEase(curva);
+        LeanTween.alphaCanvas(background.GetComponent<CanvasGroup>(), 1f, time).setEase(curva);
+        LeanTween.alphaCanvas(background2.GetComponent<CanvasGroup>(), 1f, time).setEase(curva);
+        LeanTween.alphaCanvas(texto.GetComponent<CanvasGroup>(), 1f, time).setEase(curva);
+        LeanTween.moveLocalY(cubo, 1f, time).setEase(curva).setOnComplete(() => {
+            LeanTween.moveLocalZ(cubo, -4f, time).setEase(curva);
+        });
+    }
+    public void HidePopup()
+    {
+        LeanTween.moveLocalY(background, 900, time).setEase(curva);
+        LeanTween.alphaCanvas(background.GetComponent<CanvasGroup>(), 0f, time).setEase(curva);
+        LeanTween.moveLocalX(background2, -9000, time).setEase(curva);
+        LeanTween.alphaCanvas(background2.GetComponent<CanvasGroup>(), 0f, time).setEase(curva);
+        LeanTween.alphaCanvas(texto.GetComponent<CanvasGroup>(), 0f, time).setEase(curva);
+        LeanTween.moveLocalY(cubo, -20f, time).setEase(curva);
+        LeanTween.moveLocalZ(cubo, 20f, time).setEase(curva);
+        LeanTween.moveLocalY(texto, -900, time).setEase(curva).setOnComplete(()=> {
+            background.SetActive(false);
+            background2.SetActive(false);
+            texto.SetActive(false);
+        });
+    }
+
 }
